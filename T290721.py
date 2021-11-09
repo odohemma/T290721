bf4236e842e619683fe6a1555859c3b694195fe6
Emmanuel Ifunanya Odoh <33616969+odohemma@users.noreply.github.com>
2021-11-09 12:44:09 +0100

    Update 3 of T290721
    This now searches for a Wikidata item using aliases (in addition to labels), and attempts the fifth requirement which is stated thus:
    
    "Bonus: Think of other ways of finding the right Wikidata item that doesn't depend on Wikidata labels, based on other information in the article and potential Wikidata item matches."

--- T290721.py
+++ T290721.py
@@ -31,22 +31,37 @@ if wikidataEntries['search'] != []:
     results = wikidataEntries['search']
     numresults = len(results)
     returned_labels = []
+    returned_aliases = []
+    
     for i in range(0,numresults):
         returned_labels.append(results[i]['label'])
+        if 'aliases' in results[i]:
+            returned_aliases.append(results[i]['aliases'][0])    
         
-    if search_term not in returned_labels:
+    if search_term not in returned_labels and search_term not in returned_aliases:
         print ("There isn't an exact match, but see below for closely related matches:")
         for i in range(0,numresults):
             qid = results[i]['id']
             label = results[i]['label']
             print (qid + " - " + label)
             
-    else:    
+    #The block of code below ensures that the QID of the search term as inputted 
+    #is printed, if the search term matches the wikidata label.            
+    elif search_term in returned_labels:    
         for i in range(0,numresults):
             if results[i]['label'] == search_term:
                 qid = results[i]['id']
                 label = results[i]['label']
                 print ("Here is an exact match: " + qid + " - " + label)
+    
+    #The block of code below makes it possible to search for a wikidata item
+    #using aliases. An example is Dassault Rafale.               
+    elif search_term in returned_aliases:
+        for i in range(0,numresults):
+            if results[i]['aliases'][0] == search_term:
+                qid = results[i]['id']
+                aliases = results[i]['aliases'][0]
+                print ("Here is an exact match: " + qid + " - " + aliases)
 
 else:
     print ("The inputted term does not have a QID in Wikidata.")
